bc66f9c963943df92aaafc02af73855e862d0df2
malikalrizky <110241221+malikalrizky@users.noreply.github.com>
2023-02-10 17:49:53 +0700

    test

--- main.tf
+++ main.tf
@@ -4,31 +4,6 @@
   data "google_client_config" "current" {
   }
 
-  resource "google_container_node_pool" "default" {
-  name       = "default-node-pool"
-  cluster    = google_container_cluster.default.name
-  max_pods_per_node = 32
-  location = "asia-southeast2"
-
-      node_config {
-      machine_type = "e2-medium"
-      disk_size_gb = 32
-      service_account = "github@carbon-shadow-377208.iam.gserviceaccount.com"
-      oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-    }
-
-      lifecycle {
-          create_before_destroy = true
-      }
-
-      provisioner "local-exec" {
-      when    = destroy
-      command = "sleep 90"
-    }
-}
-
   resource "google_container_cluster" "default" {
     name               = "challenge-cluster"
     location           = "asia-southeast2"
