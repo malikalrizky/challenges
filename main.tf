ed5260c4a2ef28950a0f638b852eeb8c1551d7bb
malikalrizky <110241221+malikalrizky@users.noreply.github.com>
2023-02-10 13:39:19 +0700

    test

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
       node_config {
       machine_type = "e2-medium"
       disk_size_gb = 32
-      service_account = "github@${{ secrets.GCP_PROJECT_ID }}.iam.gserviceaccount.com"
+      service_account = "github@carbon-shadow-377208.iam.gserviceaccount.com"
       oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
@@ -34,7 +34,7 @@
 
   node_config {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = "github@${{ secrets.GCP_PROJECT_ID }}.iam.gserviceaccount.com"
+    service_account = "github@carbon-shadow-377208.iam.gserviceaccount.com"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
